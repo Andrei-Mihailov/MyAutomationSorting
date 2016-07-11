cd9beb50b5360603d51ffc47c64974db032b3748
Andrey <andrew19220@yandex.ru>
2016-07-11 15:37:09 +0300

    .

---
+++ AutomationSorting/AutomationSortingService.cs
@@ -0,0 +1,32 @@
+﻿using AutomationSorting.ConveyorProcessing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationSorting
+{
+    public partial class AutomationSortingService : ServiceBase
+    {
+        public Conveyor _conveyor = null;
+        public AutomationSortingService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender1, certificate, chain, errors) => true;
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ AutomationSorting/Data/RepositoryFactory.cs
@@ -0,0 +1,27 @@
+﻿using AutomationSorting.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationSorting.Data
+{
+    public static class RepositoryFactory
+    {
+        public static IUpcRepository GetUpcRepository()
+        {
+            return new LocalDBSortingRepository();
+        }
+
+        public static ISlpRepository GetSlpRepository()
+        {
+            return new LocalDBSortingRepository();
+        }
+
+        public static IProductRepository GetProductRepository()
+        {
+            return new LocalDBSortingRepository();
+        }
+    }
+}
